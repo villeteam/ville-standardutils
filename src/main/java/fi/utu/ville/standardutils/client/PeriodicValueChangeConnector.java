04ff629875807da6fdc76a4378cc0726a823599f
Ali Leino <almale@utu.fi>
2014-12-08 12:11:07 +0200

    Probably fixes issues with text disappearing from VilleTextField and resynchronization issues which were caused by PeriodicValueChangeConnector

--- src/main/java/fi/utu/ville/standardutils/client/PeriodicValueChangeConnector.java
+++ src/main/java/fi/utu/ville/standardutils/client/PeriodicValueChangeConnector.java
@@ -1,7 +1,12 @@
 package fi.utu.ville.standardutils.client;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.RepeatingCommand;
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.event.logical.shared.AttachEvent.Handler;
 import com.vaadin.client.ComponentConnector;
 import com.vaadin.client.ServerConnector;
 import com.vaadin.client.extensions.AbstractExtensionConnector;
@@ -16,6 +21,7 @@ public class PeriodicValueChangeConnector extends AbstractExtensionConnector {
 	private static final long serialVersionUID = -2732221560697L;
             
 	private VTextField textField;
+	private boolean isRunning = false;
 
 	@Override
 	protected void extend(ServerConnector target) {
@@ -23,13 +29,35 @@ public class PeriodicValueChangeConnector extends AbstractExtensionConnector {
 	    textField = (VTextField) ((ComponentConnector) target).getWidget();
 //	    textField.addKeyPressHandler(keyPressHandler);
 	    final VTextField field = textField;
-	    
+	    textField.addAttachHandler(new Handler() {
+			
+			@Override
+			public void onAttachOrDetach(AttachEvent event) {
+				if(event.isAttached()) {
+					runCommand(field);
+				}
+				
+			}
+		});
+
+	}
+	
+	private void runCommand(final VTextField field) {
+		if(isRunning) {
+			return;
+		}
+		isRunning = true;
 	    Scheduler.get().scheduleFixedPeriod(new RepeatingCommand() {
 			String lastValue = null;
 			int count = 0;
 			
 			@Override
 			public boolean execute() {
+				
+				if(!field.isAttached()) {
+					isRunning = false;
+					return false;
+				}
 				count++;
 				String newValue = field.getText();
 				if(newValue.equals(lastValue)) {
--- src/main/java/fi/utu/ville/standardutils/client/VilleTextFieldConnector.java
+++ src/main/java/fi/utu/ville/standardutils/client/VilleTextFieldConnector.java
@@ -4,9 +4,10 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.client.ui.textfield.TextFieldConnector;
 import com.vaadin.shared.ui.Connect;
+import com.vaadin.shared.ui.Connect.LoadStyle;
 
 
-@Connect(fi.utu.ville.standardutils.ui.VilleTextField.class)
+@Connect(value =fi.utu.ville.standardutils.ui.VilleTextField.class, loadStyle=LoadStyle.EAGER)
 public class VilleTextFieldConnector extends TextFieldConnector {
 
 	private static final long serialVersionUID = 5162644024572013961L;
