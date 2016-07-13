b89ef29708bffd057e8bcb78dcd70f2a5c81f485
Ali Leino <almale@utu.fi>
2016-07-13 14:37:32 +0300

    Added a stylename to GraphLayout

--- src/main/java/edu/vserver/ville/JSXGraph/GraphLayout.java
+++ src/main/java/edu/vserver/ville/JSXGraph/GraphLayout.java
@@ -12,13 +12,13 @@ import com.vaadin.ui.VerticalLayout;
 import edu.vserver.ville.MathJax.VilleMathJax;
 
 public class GraphLayout extends VerticalLayout {
-
+	
 	private static final long serialVersionUID = 815293195464420655L;
 
 	private VilleMathJax mjax = null;
 	private String value;
-	
-//	private static CodeHighlight codeHighlight = new CodeHighlight();
+
+	//	private static CodeHighlight codeHighlight = new CodeHighlight();
 
 	@SuppressWarnings("unused")
 	private GraphLayout() {
@@ -32,7 +32,7 @@ public class GraphLayout extends VerticalLayout {
 	}
 	
 	public String getValue() {
-		return this.value;
+		return value;
 	}
 	
 	public void setValue(String value) {
@@ -42,64 +42,64 @@ public class GraphLayout extends VerticalLayout {
 	
 	public void doLayout() {
 		// Clean up before drawing.
-		this.removeAllComponents();
+		removeAllComponents();
 		
 		TeXParser texParser = new TeXParser(value);
-		Iterator<TeXBlock> blockIter =
-				texParser.iterator();
-		while(blockIter.hasNext()) {
-
+		Iterator<TeXBlock> blockIter = texParser.iterator();
+		while (blockIter.hasNext()) {
+			
 			TeXBlock block = blockIter.next();
 
-			if(block.blockType == BlockType.LATEX)
+			if (block.blockType == BlockType.LATEX) {
 				appendLatex(block.blockString);
-
-			else if(block.blockType == BlockType.ESCAPE_BLOCK)
+			} else if (block.blockType == BlockType.ESCAPE_BLOCK) {
 				appendGraph(block.blockString);
+			}
 
 		}
 		
-		this.addStyleName("ville-graphlayout");
+		addStyleName("ville-graphlayout");
 		mjax = new VilleMathJax("ville-graphlayout");
-		
+		mjax.setStyleName("mjax-invisible");
 		this.addComponent(mjax);
 	}
 
 	private void appendLatex(String latexString) {
 		//String[] paragraphs = latexString.split("\\r?\\n");
 		//for(String s : paragraphs) {
-			Label latexLabel = new Label(latexString, ContentMode.HTML);
-			latexLabel.addStyleName("readability");
-			this.addComponent(latexLabel);
+		Label latexLabel = new Label(latexString, ContentMode.HTML);
+		latexLabel.addStyleName("readability");
+		this.addComponent(latexLabel);
 		//}
 	}
 
 	private void appendGraph(String blockString) throws IllegalArgumentException {
-
+		
 		JSXGraphComponentFactory fac = new JSXGraphComponentFactory();
 		try {
-
+			
 			HashMap<String, String> blockContents = EscapeBlockParser.parseBlock(blockString);
 			
 			switch (blockContents.get("type")) {
-				case "parametricPlot": case "linePlot": case "pointPlot": case "functionPlot":
-					JSXGraphComponent graph = fac.makeGraph(blockContents);
-					graph.addStyleName("readability");
-					this.addComponent(graph);
-					this.setComponentAlignment(graph, Alignment.MIDDLE_CENTER);
+			case "parametricPlot":
+			case "linePlot":
+			case "pointPlot":
+			case "functionPlot":
+				JSXGraphComponent graph = fac.makeGraph(blockContents);
+				graph.addStyleName("readability");
+				this.addComponent(graph);
+				setComponentAlignment(graph, Alignment.MIDDLE_CENTER);
 				break;
-				default:
-					Notification.show(
+			default:
+				Notification.show(
 						"Unhandled escape block type '" + blockContents.get("type") + "' in '" + blockString + "'.",
-						Notification.Type.WARNING_MESSAGE
-					);
+						Notification.Type.WARNING_MESSAGE);
 				break;
 			}
-		} catch(IllegalArgumentException iae) {
-
+		} catch (IllegalArgumentException iae) {
+			
 			Notification.show("Illegal arguments: " +
-				iae.getMessage()
-				, Notification.Type.WARNING_MESSAGE);
+					iae.getMessage(), Notification.Type.WARNING_MESSAGE);
 
 		}
 
