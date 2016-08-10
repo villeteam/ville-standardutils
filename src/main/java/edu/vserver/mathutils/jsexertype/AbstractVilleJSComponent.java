1951afe5e38ab2c5fdcf1df6102037fb659feda2
Niko Hellgren <nipehe@utu.fi>
2016-08-10 10:30:23 +0300

    Added an empty JS function to avoid error messages during JS component creation, version++

--- src/main/java/edu/vserver/mathutils/jsexertype/AbstractVilleJSComponent.java
+++ src/main/java/edu/vserver/mathutils/jsexertype/AbstractVilleJSComponent.java
@@ -3,14 +3,15 @@ package edu.vserver.mathutils.jsexertype;
 import com.vaadin.ui.AbstractJavaScriptComponent;
 
 @com.vaadin.annotations.JavaScript({
-		"public/compatible_versions/jquery.min.js",
-		"public/compatible_versions/jquery-ui.min.js",
-		"public/compatible_versions/jquery-ui-touch.min.js",
-		"public/compatible_versions/raphael.js",
-		"public/highlight.js/highlight.pack.js",
-		"public/compatible_versions/underscore-min.js" })
+	"public/init.js",
+	"public/compatible_versions/jquery.min.js",
+	"public/compatible_versions/jquery-ui.min.js",
+	"public/compatible_versions/jquery-ui-touch.min.js",
+	"public/compatible_versions/raphael.js",
+	"public/highlight.js/highlight.pack.js",
+	"public/compatible_versions/underscore-min.js" })
 public class AbstractVilleJSComponent extends AbstractJavaScriptComponent {
-
+	
 	private static final long serialVersionUID = -8140588014611693869L;
-
+	
 }
---
+++ src/main/java/edu/vserver/mathutils/jsexertype/public/init.js
@@ -0,0 +1,2 @@
+/* Empty function to avoid error messages */
+edu_vserver_mathutils_jsexertype_AbstractVilleJSComponent = function() {};
